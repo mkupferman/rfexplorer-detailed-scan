fe312eea6c661a8276db4675acf20ce9ac89f9f1
Matt Kupferman <mkupferman@usnews.com>
2024-07-05 16:37:09 -0400

    increment version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="rfexplorer-detailed-scan",
-    version="0.0.1",
+    version="0.0.2",
     py_modules=[
         "rfexplorerDetailedScan",
         "rfexplorerDetailedScanLib",
